cd3fb337c4c18339115f2be859c0393b39522a51
Angello854 <amonper2901@g.educaand.es>
2025-03-16 15:49:23 +0000

    Fondo

---
+++ proyecto/core/src/main/java/com/space/invaders/model/Fondo.java
@@ -0,0 +1,45 @@
+package com.space.invaders.model;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Fondo {
+    private float ancho;
+    private float largo;
+    private float x;
+    private float y;
+    private float velocidadFondo;
+    private Texture tableroInteractivo;
+
+    public Fondo(float velocidadFondo,Texture tableroInteractivo) {
+        this.ancho = Gdx.graphics.getWidth();
+        this.largo = Gdx.graphics.getHeight();
+        this.x = 0;
+        this.y = 0;
+        this.velocidadFondo = velocidadFondo;
+        this.tableroInteractivo = new Texture ("fondo.jpeg");
+    }
+
+    public float getVelocidadFondo() {
+        return velocidadFondo;
+    }
+    public void setVelocidadFondo (float velocidadFondo) {
+        this.velocidadFondo = velocidadFondo;
+    }
+
+    public void dibujarFondo (SpriteBatch batch) {
+        batch.draw(tableroInteractivo,x,y,ancho,largo);
+        batch.draw(tableroInteractivo,x,y + Gdx.graphics.getHeight(),ancho,largo);
+    }
+    public void moverFondo () {
+        y = y - (velocidadFondo * Gdx.graphics.getDeltaTime());
+
+        if (y <= -Gdx.graphics.getHeight()) {
+            y = 0;
+        }
+    }
+
+
+
+}
